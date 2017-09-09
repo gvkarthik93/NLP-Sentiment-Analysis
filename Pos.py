658c0ec0adaeb294fe7a7b56a8afabd43b6610b8
Monica Lakshmi Sunkara <31045150+monicasunkara@users.noreply.github.com>
2017-09-09 18:12:13 -0400

    Add files via upload

--- Pos.py
+++ Pos.py
@@ -1,4 +1,6 @@
 import random
+import numpy as np
+import random
 
 def unigramFunction(corpusList):
 	for x in corpusList:
@@ -13,25 +15,79 @@ def bigramFunction(corpusList):
 	return bigramDictionary
 
 f = open("/Users/monicasunkara/Downloads/Project1/SentimentDataset/Dev/pos.txt")
+
 corpusList = list()
 bigramList = list()
+uniqueCorpusList = list()
 
 unigramDictionary = dict()
 bigramDictionary = dict()
 
+# Generating corpus list
 for line in f:
+	line = "<s>" + " " + line + " " + "</s>"
 	corpusList = corpusList + line.split()
 	lineList = line.split()
 	
 	for i in range(0,len(lineList)-1):
-		bigramList.append(lineList[i]+lineList[i+1])
+		bigramList.append(lineList[i]+ " " + lineList[i+1])
+
+uniqueCorpusList = list(set(corpusList));
+
+#Bigram Probability Matrix
+corpusUniqueWordCount = len(uniqueCorpusList);
+corpusWordCount = len(corpusList);
+
+print (corpusWordCount)
+print (corpusUniqueWordCount)
+
+#bigramMatrix = np.zeros((corpusUniqueWordCount, corpusUniqueWordCount), np.float64);
+bigramMatrix = np.zeros((10,10), np.float64);
+
+print(uniqueCorpusList[0])
+for i in range(0, 10):
+	for j in range(0, 10):
+		word1 = uniqueCorpusList[i];
+		countOfWord = corpusList.count(word1);
+		word2 = uniqueCorpusList[j];
+		concatWord = word1 + " " + word2;
+		countOfConcatWord = bigramList.count(concatWord);
+		bigramProb = countOfConcatWord/countOfWord;
+		bigramMatrix[i][j] = bigramProb;
+
+# Unigram Random Sentence Generator
 count = 0
 randomSentence = ""
-while (count < 10):
-	randomSentence = randomSentence + " " + random.choice(corpusList)
+while (count < 10000):
+	randomWord = random.choice(corpusList)
+	if randomWord == "</s>":
+		break
+	randomSentence = randomSentence + " " + randomWord;
+	count = count + 1;
+
+#Bigram Random Sentence Generator
+count = 0
+prev;
+bigramRandomSentence = ""
+while (count < 100):
+	print (prevWord)
+	indexOfPrev = uniqueCorpusList.index(prevWord)
+	print(indexOfPrev)
+	iterate = 0;
+	for prob in bigramMatrix[indexOfPrev]:
+		word2 = uniqueCorpusList[iterate];
+		my_list = my_list + [word2] * prob;
+		iterate = iterate + 1;
+	print(my_list)
+	randomWord = random.choice(my_list);
+	if randomWord == "</s>":
+		break;
+	prevWord = randomWord;
+	bigramRandomSentence = bigramRandomSentence + " " + randomWord
 	count = count + 1
 
-print (randomSentence)
+print(bigramRandomSentence)
+
 #print (unigramFunction(corpusList))
 #print (bigramFunction(bigramList))
 
