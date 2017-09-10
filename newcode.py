80b9ae76355252ab1902b58e3f6383ad12680b79
Monica Lakshmi Sunkara <31045150+monicasunkara@users.noreply.github.com>
2017-09-10 00:20:54 -0400

    Add files via upload

---
+++ newcode.py
@@ -0,0 +1,58 @@
+import collections
+f = open("/Users/monicasunkara/Downloads/Fall2017/NLP-Sentiment-Analysis/SentimentDataset/Dev/pos.txt")
+unigram = collections.defaultdict(int)
+
+bigram = {}
+
+# word_count = collections.defaultdict(int)
+
+# collections.defaultdict
+
+# fopen
+
+for line in f:
+    line = ["<s>"]+line.split() + ["</s>"]
+    # for word in line:
+    #   # unigram[word] += 1
+    #   pass
+    for i in range(len(line)-1):
+        word, next_word = line[i], line[i+1]
+        unigram[word]+=1
+        
+        if word not in bigram:
+            # bigram[word] = collections.defaultdict(int)
+            bigram[word] = {}
+        if next_word not in bigram[word]:
+            bigram[word][next_word] = 0
+        bigram[word][next_word]+=1
+
+#  generate sentence
+
+import random
+
+# Unigram Random Sentence Generator
+count = 0
+randomSentence = ""
+while (count < 10000):
+    randomWord = random.choice(unigram.keys)
+    if randomWord == "</s>":
+        break
+    randomSentence = randomSentence + " " + randomWord;
+    count = count + 1;
+
+#Bigram Random Sentence Generator
+count = 0
+prevWord = "<s>";
+bigramRandomSentence = ""
+while (count < 100):
+    iterate = 0;
+    for key, value in bigram[prevWord].iteritems():
+        my_list = my_list + [key] * value;
+    randomWord = random.choice(my_list);
+    if randomWord == "</s>":
+        break;
+    prevWord = randomWord;
+    bigramRandomSentence = bigramRandomSentence + " " + randomWord
+    count = count + 1
+
+print(bigramRandomSentence)
